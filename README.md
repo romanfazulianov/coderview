13510856be2b6ee8cc6361ef0b61c24911bc54ac
Roman Fazulianov <faz.dev@yandex.ru>
2016-09-07 09:54:44 +0400

    init repo for react

---
+++ README.md
@@ -0,0 +1,12 @@
+#My CoderView Site
+
+    git clone https://github.com/romanfazulianov/coderview.git
+    npm install
+
+then for futher development:
+
+    npm run dev
+
+or for production
+
+    npm run prod
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+
+module.exports = {
+    entry: './app/App.js',
+    output: {
+        filename: 'public/bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel',
+                query: {
+                    presets: ['react', 'es2015']
+                }
+            },
+        ]
+    },
+    plugins: [
+        new BrowserSyncPlugin({
+            // browse to http://localhost:3000/ during development,
+            // ./public directory is being served
+            proxy: 'localhost:3100',
+            host: 'localhost',
+            port: 3000,
+            // server: {
+            //   baseDir: ['./public'],
+            //   index: 'index.html'
+            // }
+        })
+    ]
+};
