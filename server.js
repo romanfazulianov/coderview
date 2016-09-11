f4917077766c20b2093a8a561e2808f3cf8ee22e
Roman Fazulianov <faz.dev@yandex.ru>
2016-09-11 10:26:39 +0400

    add polyfill and clear comments in webpack.config

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const app = express();
 const path = require("path");
 const port = 3100;
 const address = `http://localhost:${port}/`;
-const staticGzip = require('static-gzip');
-app.use(staticGzip("dist"));
+
+app.use(express.static("dist"));
 
 app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "dist/index.html"));
--- webpack.config.js
+++ webpack.config.js
@@ -6,11 +6,13 @@ const isDebugMode = NODE_ENV === 'development';
 const webpack = require('webpack');
 const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-// const CompressionPlugin = require("compression-webpack-plugin");
 
 const config = {
 
-    entry: './app/App.js',
+    entry: [
+        'babel-polyfill',
+        './app/App.js'
+    ],
     output: {
         filename: 'dist/bundle.js'
     },
@@ -21,10 +23,8 @@ const config = {
                 exclude: /node_modules/,
                 loader: 'babel',
                 query: {
-                //     plugins: [
-                //         'transform-runtime'
-                // ],
-                    presets: ['react', 'es2015']
+                    plugins: ['transform-runtime'],
+                    presets: ['react', 'es2015', 'stage-0']
                 }
             },
             {
@@ -39,43 +39,19 @@ const config = {
     },
     devtool: isDebugMode ? 'inline-cheap-module-source-map' : null,
     plugins: [
-
         new webpack.NoErrorsPlugin(),
         new ExtractTextPlugin('dist/styles/main.css', {
             allChunks: true
         }),
-        // new webpack.optimize.CommonsChunkPlugin({
-        //     children: true,
-        //     async: true,
-        // }),
-
-        // new webpack.optimize.DedupePlugin(),
-        // new webpack.optimize.OccurrenceOrderPlugin(),
-        // new CompressionPlugin({
-        //     asset: "[path].gz[query]",
-        //     algorithm: "gzip",
-        //     test: /\.js$|\.html$/,
-        //     threshold: 10240,
-        //     minRatio: 0.8
-        // })
-        // new webpack.DefinePlugin({
-        //     NODE_ENV: JSON.stringify(NODE_ENV)
-        // })
     ]
 };
 
 if (isDebugMode) {
     config.plugins.push(
         new BrowserSyncPlugin({
-            // browse to http://localhost:3000/ during development,
-            // ./public directory is being served
             proxy: 'localhost:3100',
             host: 'localhost',
             port: 3000,
-            // server: {
-            //   baseDir: ['./public'],
-            //   index: 'index.html'
-            // }
         })
     );
 }
@@ -86,9 +62,9 @@ if (isProductionMode) {
             beautify: false,
             comments: false,
             compress: {
-                sequences     : true,
-                booleans      : true,
-                loops         : true,
+                sequences   : true,
+                booleans    : true,
+                loops       : true,
                 unused      : true,
                 warnings    : false,
                 drop_console: true,
